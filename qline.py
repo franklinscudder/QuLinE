5ae2b02abf1807c849bb9343a90d122c3bb7647d
Tom Findlay <findlaytel@gmail.com>
2021-03-29 06:20:01 +0100

    Update qline.py

--- qline.py
+++ qline.py
@@ -9,25 +9,22 @@ Ax = b
 
 """
 
-D = 2  #equation dimension
-A = np.eye(D) # ????????
 
-t = D * 2   # bits in b
+A = np.kron(np.array([[1,0],[0,1]]),np.array([[1+0j, 1j], [-1j, 0j]])) # ????????
 
-b = register(t)  # 2 bits per value in b, value of B is now [8, 0]?
+t = 4   # bits in b
+b = register(t)  
 T = 2 ** t    # states in b
 amps = np.sqrt(2/T) * np.array([np.sin((np.pi*(tau+0.5)/T)) for tau in range(T)])
 phi0 = register(t)
 phi0.setAmps(amps)
 
-
-
 phi0b = prod(phi0, b)
-
 t0 = 1
-hamMat = []
 
-for tau in range(T):
+hamMatTerms = []
+
+for tau in range(T):                    #construct hamilton operator
     tautau = np.zeros((T, T))
     tautau[tau, tau] = 1
     print("tautau: ", tautau.shape)
@@ -35,10 +32,12 @@ for tau in range(T):
     print("oper: \n", oper)
     term = np.kron(tautau, oper)
     print("TERM: ", term.shape)
-    hamMat.append(term)
+    hamMatTerms.append(term)
 
 print()
-hamMat = np.sum(hamMat, axis=0) 
-print(hamMat.shape)
-print(phi0b.NStates)
+hamMat = np.sum(hamMatTerms, axis=0)   
+print(hamMat.shape)                    # these should match...
+print(phi0b.NStates)                   # ... but they dont
+ham = genericGate(phi0b.NBits)        #make it a gate
+ham.matrix = hamMat
 
