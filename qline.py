61a5ab2e05026967448ade021604479f3561e93b
Tom Findlay <findlaytel@gmail.com>
2021-03-29 05:51:21 +0100

    Update qline.py

--- qline.py
+++ qline.py
@@ -1,34 +1,48 @@
 
 from qutiepy import *
 import numpy as np
+from scipy.linalg import expm
 
 """
 
-Cx = b
+Ax = b
 
 """
 
-C = np.eye(2)
+D = 2  #equation dimension
+A = np.eye(D) # ????????
 
-t = 4
+t = 4   # bits in b
 
 b = register(t)  # 2 bits per value in b, value of B is now [8, 0]?
-T = 2 ** t
+T = 2 ** t    # states in b
 amps = np.sqrt(2/T) * np.array([np.sin((np.pi*(tau+0.5)/T)) for tau in range(T)])
 phi0 = register(t)
 phi0.setAmps(amps)
 
 
-print(phi0)
 
 phi0b = prod(phi0, b)
 
 t0 = 1
+hamMat = []
+
 for tau in range(T):
-    amps = np.zeros(T, dtype=np.dtype(complex))
-    amps[tau] = phi0b.amps[tau]
-    print(amps)
-    kettau = register(t)
-    kettau.setAmps(amps)
-    tautau = prod(kettau, kettau)
-    
+    tau_ = np.zeros((T, 1))
+    tau_[:,0] = np.array([int(i == tau) for i in range(T)])
+    print("tau_: ", tau_.shape)
+    tau_T = tau_.T
+    print("tau_T: ", tau_T.shape)
+    tautau = np.kron(tau_, tau_T)
+    print("tautau: ", tautau.shape)
+    oper = expm(1j*tau*t0*A/T)
+    print("oper: \n", oper)
+    term = np.kron(tautau, oper)
+    print("TERM: ", term.shape)
+    hamMat.append(term)
+
+print(np.array(hamMat).shape)
+hamMat = np.sum(hamMat, axis=0) 
+print(hamMat.shape)
+print(phi0b.NStates)
+
