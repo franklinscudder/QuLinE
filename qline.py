a3a37c361c751aee10afb6ad6659970cdd8fa280
Tom Findlay <findlaytel@gmail.com>
2021-03-29 07:19:20 +0100

    Update qline.py

--- qline.py
+++ qline.py
@@ -10,27 +10,33 @@ Ax = b
 """
 
 
-A = np.kron(np.array([[1,0],[0,1]]),np.array([[1+0j, 1j], [-1j, 0j]])) # ????????
+# ????????
 
 t = 4   # bits in b
-b = register(t)  
-T = 2 ** t    # states in b
+A = np.eye(t)
+b = register(4)  
+T = 2 ** t    # states in b = 16
 amps = np.sqrt(2/T) * np.array([np.sin((np.pi*(tau+0.5)/T)) for tau in range(T)])
 phi0 = register(t)
 phi0.setAmps(amps)
-
+#print(len(phi0.amps))  ## 16
+print(b)
+input()
 phi0b = prod(phi0, b)
+#print(phi0b.NStates)  ## 256??? should be tT???
+#print(len(np.kron(phi0.amps, b.amps)))
+#input()
 t0 = 1
 
 hamMatTerms = []
 
 for tau in range(T):                    #construct hamilton operator
     tautau = np.zeros((T, T))
-    tautau[tau, tau] = 1
+    tautau[tau, tau] = 1             # t x t
     print("tautau: ", tautau.shape)
-    oper = expm(1j*tau*t0*A/T)
-    print("oper: \n", oper)
-    term = np.kron(tautau, oper)
+    oper = expm(1j*tau*t0*A/T)      # t x t
+    print("oper: \n", oper.shape)
+    term = np.kron(tautau, oper)    # tT x tT
     print("TERM: ", term.shape)
     hamMatTerms.append(term)
 
